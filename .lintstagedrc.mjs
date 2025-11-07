b276e42205faf935fcdde7cc632343cbef1d04bb
dominik.kielbowicz <dominik.kielbowicz19@gmail.com>
2025-11-07 14:44:06 +0100

    chore: init, setup linters and tests

---
+++ .lintstagedrc.mjs
@@ -0,0 +1,9 @@
+import path from "path";
+
+const buildEslintCommand = (filenames) =>
+    `eslint --fix ${filenames
+        .map((f) => `"${path.relative(process.cwd(), f)}"`)
+        .join(" ")}`;
+export default {
+    "*.{js,jsx,ts,tsx}": [buildEslintCommand, "prettier -c"],
+};
---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const config: Config = {
+    roots: ["<rootDir>/tests/unit"],
+    coverageProvider: "v8",
+    testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config);
